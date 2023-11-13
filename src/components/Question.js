54eeb8b41996664d4041009df6fba63ff0e46f09
jeffmanhart <jeffreymanhart@gmail.com>
2023-11-13 10:08:47 -0700

    Got the timer to countdown and clear when reaches 0

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,9 +1,19 @@
-import React, { useState } from "react";
+import React, { useEffect , useState } from "react";
 
 function Question({ question, onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
 
   // add useEffect code
+  useEffect(()=>{
+    const timerId = setTimeout(() => {
+    if(timeRemaining===1) {
+      onAnswered(false);
+      setTimeRemaining(10);
+      
+    }else
+      {setTimeRemaining(timeRemaining-1)}}, 1000)
+    return ()=>clearTimeout(timerId)
+  })
 
   function handleAnswer(isCorrect) {
     setTimeRemaining(10);
